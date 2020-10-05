52bfe16b415155f4666851dbcadc3d0bde50fef4
lius6219 <72394277+lius6219@users.noreply.github.com>
2020-10-05 23:57:33 +0800

    Update main.py

--- Test/main.py
+++ Test/main.py
@@ -9,9 +9,4 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print "Hi, {0}".format(name)  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm, 得的')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# github 远端修改记录
